f9f2f72711d899bd64ea436dc3cb3a691f6ca369
Jhun Gulle <71265836+jeody@users.noreply.github.com>
2024-10-02 13:33:09 +0800

    email sentv2 commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -11,9 +11,6 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
   });
 
   // Options for sending email
